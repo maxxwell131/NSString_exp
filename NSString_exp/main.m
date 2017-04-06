b6981040020985c3fec65d0ebf5730f0737ab450
maxxwell131 <maxxwell131@yahoo.com>
2017-04-06 13:30:51 +0300

    First commit

--- NSString_exp/main.m
+++ NSString_exp/main.m
@@ -12,6 +12,12 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        char *myString = "This is a C character string";
+        char myStringArray[] = "This is a C character string";
+        
+        NSLog(@"Length myStringArray[] %lu", sizeof(myStringArray)/sizeof(*myStringArray)); //29
+        NSLog(@"Length myString %lu", strlen(myString)); //28
     }
     return 0;
 }

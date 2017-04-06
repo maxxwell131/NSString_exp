2d8ffae250de7be7fd19270a2323cae8985f9069
admin <admin@MacBook-Pro-admin.local>
2017-04-06 13:06:08 +0300

    Initial Commit

---
+++ NSString_exp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSString_exp
+//
+//  Created by admin on 4/6/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
